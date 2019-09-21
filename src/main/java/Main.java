82057a085ff2b5834a82573bca7ea3f7cf4e84c2
Student <student@itpark.ru>
2019-09-21 19:27:52 +0300

    Реализация класса Post

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        System.out.println(post.id);
+        System.out.println(post.removed);
+        post.id = 1;
+        System.out.println(post.id);
+        char firstLetter = 'A';
+        char newLine = '\n';
+        String name = "IT Academy";
+        System.out.println(name);
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    long id;
+    String avatarUrl;
+    String author;
+    int created;
+    String content;
+    int likes;
+    int comments;
+    int views;
+    boolean removed;
+
+}
